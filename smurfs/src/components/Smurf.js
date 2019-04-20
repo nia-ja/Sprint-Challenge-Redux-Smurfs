d93949f45024bab91712d107ad418e721b92289d
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-04-20 11:24:38 -0700

    Switched deleteSmurf action from the Smurf card to delete control

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -2,10 +2,16 @@ import React from "react";
 
 const Smurf = props => {
     return (
-        <div className="smurf" onClick={() => props.onClick(props.smurf.id)}>
-            <p>Name: {props.smurf.name}</p>
-            <p>Age: {props.smurf.age}</p>
-            <p>Height: {props.smurf.height}</p>
+        <div className="smurf">
+            <div className="smurf-controls">
+                <button onClick={() => props.onClick(props.smurf.id)}>✘</button>
+                <button>✎</button>
+            </div>
+            <div className="smurf-data">
+                <p>Name: {props.smurf.name}</p>
+                <p>Age: {props.smurf.age}</p>
+                <p>Height: {props.smurf.height}</p>
+            </div>
         </div>
     )   
 };
